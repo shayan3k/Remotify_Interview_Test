3164318ae3ca3e8d9ec9f3ca82e8cec73675b649
shayan <shayan3k@gmail.com>
2022-06-03 22:20:53 +0430

    Updated

--- app/app/Console/Commands/FetchMergeData.php
+++ app/app/Console/Commands/FetchMergeData.php
@@ -47,20 +47,57 @@ class FetchMergeData extends Command
     public function handle()
     {
 
-
-        $response = Http::withHeaders([
+        // get the list of employees
+        $employees = Http::withHeaders([
             'x-token' => 'removify',
             'Content-Type' => 'application/json'
         ])->get('https://service-dev.rmvfy.com/interview?source=db');
 
 
+        // get the list of employees
+        $companies = Http::withHeaders([
+            'x-token' => 'removify',
+            'Content-Type' => 'application/json'
+        ])->get('https://service-dev.rmvfy.com/interview?source=companies');
 
-        if ($response->failed()) {
-            return "Could not Locate the Endpoint Server.";
+        // check if the request has been fail
+        if ($employees->failed() || $companies->failed()) {
+            echo "Could not Locate the Endpoint Server.";
         } else {
-            echo "hello world!";
-            echo $response;
-            // return success
+
+            // convert the request body to array
+            $companies =  json_decode($companies->getBody(), true);
+            $companies = $companies['data'];
+
+            // convert the request body to array
+            $employees =  json_decode($employees->getBody(), true);
+            $employees = $employees['data'];
+
+            // print table header
+            echo "id\t\tname\t\ttitle\t\tcompany_name\n";
+            echo "--\t\t----\t\t-----\t\t------------\n";
+
+            // print the information
+            foreach ($employees as $key => $item) {
+
+                $companyName = $this->getCompanyName($companies, $item['company_id']);
+                echo $item['id'] . "\t\t" . $item['name'] . "\t\t" . $item['title'] . "\t\t" . $companyName  . "\n";
+            }
+        }
+    }
+
+
+    /**
+     * Get the Company Name with Comapny Id
+     *
+     * @return int
+     */
+    public function getCompanyName($companies, $company_id)
+    {
+        // print the information
+        foreach ($companies as $key => $item) {
+            if ($item['company_id'] == $company_id)
+                return $item['company_name'];
         }
     }
 }
